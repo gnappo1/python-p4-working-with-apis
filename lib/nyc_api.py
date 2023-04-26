f5a6b3c101c11cd358ea7bf8d18db57139a6ecdb
Matteo Piccini <gnappo.1@live.it>
2023-04-26 16:08:46 -0700

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -3,12 +3,17 @@ import json
 
 class GetPrograms:
 
-  def get_programs():
+  def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
     return response.content
 
+  def program_school(self):
+    return [program['agency'] for program in json.loads(self.get_programs())]
 
-programs = GetPrograms.get_programs()
-print(programs)
+prog = GetPrograms()
+schools = prog.program_school()
+
+for school in set(schools):
+  print(school)
